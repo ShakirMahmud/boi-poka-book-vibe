6a874ad56126c33ae3630511c5bee90a6123f5c4
ShakirMahmud <shakirmahmud50@gmail.com>
2024-11-02 18:16:09 +0600

    show ratings and set book detail route

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,28 +1,38 @@
+import { AiOutlineStar } from "react-icons/ai";
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Book = ({ book }) => {
-    const { image, bookName, author, tags, rating, category } = book;
+    const {bookId, image, bookName, author, tags, rating, category } = book;
     return (
-        <div className="card bg-base-100 w-96 shadow-xl p-6">
-            <figure className='bg-[#F3F3F3] py-8 rounded-2xl'>
-                <img 
-                    src={image}
-                    alt={bookName}
-                    className='h-[166px]'
+        <Link to={`/books/${bookId}`}>
+            <div className="card bg-base-100 w-96 shadow-xl p-6">
+                <figure className='bg-[#F3F3F3] py-8 rounded-2xl'>
+                    <img
+                        src={image}
+                        alt={bookName}
+                        className='h-[166px]'
                     />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {bookName}
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>By: {author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                </figure>
+                <div className="card-body">
+                    <div className='flex justify-center gap-4'>
+                        {
+                            tags.map(tag => <button className="btn bg-green-100 text-green-900 btn-xs">{tag}</button>)
+                        }
+                    </div>
+                    <h2 className="card-title">
+                        {bookName}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>By: {author}</p>
+                    <div className="border-t-2 border-dashed mt-4"></div>
+                    <div className="card-actions justify-between mt-4">
+                        <div className="badge badge-outline">{category}</div>
+                        <div className="badge badge-outline">{rating} <span className="mr-2"> </span>  <AiOutlineStar /></div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
